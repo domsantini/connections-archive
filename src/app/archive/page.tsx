be68aa27faa9892c9a940f682b6caf181d270a54
domsantini <111545540+domsantini@users.noreply.github.com>
2024-08-14 11:05:20 -0700

    Fix static archive page

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import React from 'react'
 import PuzzleList from '../components/PuzzleList';
 import useGetPuzzleId from "@/app/hooks/use-get-puzzle-id";
